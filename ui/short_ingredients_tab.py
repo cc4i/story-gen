d5674885f992558670775de063bb16881d3d404e
Aria Ho <ariaho@google.com>
2025-10-15 20:18:19 +0000

    Preliminary implementation of upload button for tab 4

--- ui/short_ingredients_tab.py
+++ ui/short_ingredients_tab.py
@@ -6,5 +6,8 @@ def short_ingredients_tab():
         with gr.Row():
             short_ingredients=gr.Gallery(label="Generated videos", type="filepath", show_label=False, elem_id="gallery", columns=[3], rows=[4], object_fit="contain", height="auto")
         with gr.Row():
+            num_scene = gr.Number(label="Scene number", visible=False, value=1, minimum=1, maximum=12, interactive=True)
+            upload_btn = gr.UploadButton(label="Replace scene", visible=False, file_count="single", file_types=["mp4"], interactive=True)
+        with gr.Row():
             btn_merge_videos = gr.Button("Merge videos")
-    return short_ingredients, btn_merge_videos
+    return short_ingredients, btn_merge_videos, num_scene, upload_btn
